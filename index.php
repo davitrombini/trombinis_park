1a73ba3c62c5312e0d64fbdab66683eb1a43c24c
davitrombini <79776959+davitrombini@users.noreply.github.com>
2021-03-04 19:11:52 -0300

    update index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include_once('src_head.php'); ?>
+
+<body>
+   <script>
+      
+   </script>  
+   <h1>H1H1H</h1>  
+</body>
+
+</html>
---
+++ src_head.php
@@ -0,0 +1,8 @@
+<head>
+    <!--
+        Configuração do XAMPP - https://tonyfrenzy.medium.com/xampp-serving-from-any-directory-outside-of-htdocs-22a93f1b8815
+    --> 
+    <!-- jQuery -->
+    <script src = "/node_modules/jquery/dist/jquery.min.js"></script>
+
+</head>

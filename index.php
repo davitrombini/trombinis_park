23d8a4a18b3e104a5d42b2fe56686a33bf45432c
davitrombini <79776959+davitrombini@users.noreply.github.com>
2021-03-04 19:55:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
 <?php include_once('src_head.php'); ?>
 
 <body>
-   <script>
-      
-   </script>  
-   <h1>H1H1H</h1>  
-   <h2>djfpidshço</h2>
-   <h1>H1H1H</h1>   
+  
+   <button class="ui blue button">
+       Salve!
+   </button>  
 </body>
 
 </html>

8cedd14999a7072f027413917ca8afc4b17b2c44
davitrombini <79776959+davitrombini@users.noreply.github.com>
2021-03-04 19:47:01 -0300

    configs iniciais

--- src_head.php
+++ src_head.php
@@ -2,7 +2,11 @@
     <!--
         Configuração do XAMPP - https://tonyfrenzy.medium.com/xampp-serving-from-any-directory-outside-of-htdocs-22a93f1b8815
     --> 
+
     <!-- jQuery -->
     <script src = "/node_modules/jquery/dist/jquery.min.js"></script>
 
+    <!-- Fomantic Ui -->
+    <link rel="stylesheet" type="text/css" href="/semantic/dist/semantic.min.css">
+    <script src="/semantic/dist/semantic.min.js"></script>
 </head>

9aac51c38bde5fb8a7a21cb2a991150ffb317dd4
felipe <36082241+byuufx@users.noreply.github.com>
2021-04-07 01:01:01 -0300

    login start

--- adm/login.php
+++ adm/login.php
@@ -1,1 +1,52 @@
-a
+<!-- página de exemplo de estrutura a ser seguida -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php include_once('./src_head_adm.php'); ?>
+
+<style>
+      body{
+        background-color: #EEEEEE;
+      }
+</style>  
+	<body>
+
+		<section class="login">
+
+			<div class="ui grid">
+
+				<div class="five wide column"> </div>
+
+				<div class="six wide column">
+
+					<form class="ui mini form" method="POST" action="./src_login.php">
+
+						<div class="field">
+							<label for="email">Email:</label>
+							<div class="ui left icon input"> <i class="at icon"></i>
+								<input type="email" placeholder="Insira seu email" name="email"> </div>
+						</div>
+
+						<div class="field">
+							<label for="email">Senha:</label>
+							<div class="ui left icon input"> <i class="lock icon"></i>
+								<input type="password" placeholder="Insira sua senha" name="senha"> </div>
+						</div>
+
+						<button class="ui blue icon button">
+              <i class="arrow alternate circle right icon"></i>
+              Login
+            </button>
+
+					</form>
+
+				</div>
+
+				<div class="five wide column"></div>
+
+			</div>
+
+		</section>
+
+	</body>
+
+</html>
---
+++ adm/scr_login.php
@@ -0,0 +1,9 @@
+<?php
+
+
+  /*
+    $stmt = $pdo->prepare("SELECT * FROM tbl_usuarios WHERE id=:id");
+    $stmt->execute(['email' => $email]); 
+    $user = $stmt->fetch();
+  */
+?>

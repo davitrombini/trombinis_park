0589662e3f9b2c3e097a719d096b6d9ded42cad0
Renan Carlos Agostinho <renan.carlos.agostinho@gmail.com>
2021-10-01 10:34:34 -0300

    criação do form cadastro de usuários
    dei inicio a criação do form de cadastro, n sei mexer bem com o fomantic, por isso pode parecer feio, quem tiver um domínio maior e quiser modificar, pode. Também queria adicionar validação através da fomantic, mas por algum motivo a validação n está funcionando..

--- src/pages/user/create-user.php
+++ src/pages/user/create-user.php
@@ -6,25 +6,70 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../css/create-user.css">
   <title>Criar conta</title>
-  <?php include ('C:/xampp/htdocs/trombinis_park/master_head.php'); ?>
+  <link rel="stylesheet" type="text/css" href="../../../semantic/dist/semantic.min.css">
+  <script src="../../../semantic/dist/semantic.min.js"></script>
+  <script>
+
+    $('.ui.form').form({
+      fields: {
+        name: {
+          rules: [{
+            type: 'empty',
+            Prompt: 'Por favor, insira seu nome'
+          }]
+        },
+        email: {
+          rules: [{
+            type: 'empty',
+            Prompt: 'Por favor, insira seu e-mail'
+          }]
+        }
+      }
+    });
+
+  </script>
 </head>
 <body>
-  <form class="ui form" id="create-user-form">
-    <div class="required field">
-      <input type="text" name="name" maxlength="255" id="input-user-name" placeholder="Nome">
-    </div>
+  <div class="ui middle aligned center aligned grid container" style="position: relative; top: 15%">
+    <div class="column" style="width: 60%">
+      <h1 class="ui pink header">
+        <div class="content">
+          Crie sua conta
+        </div>
+      </h1>
+      <form class="ui centered form" id="create-user-form">
+        <div class="ui stacked segment" style="margin-top: 30px;">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="user icon"></i>
+              <input type="text" name="name" maxlength="255" id="input-user-name" placeholder="Nome">
+            </div>
+          </div>
+          
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="envelope icon"></i>
+              <input type="email" name="email" maxlength="255" id="input-user-email" placeholder="Email">
+            </div>
+          </div>
 
-    <div class="required field">
-      <input type="email" name="email" maxlength="255" id="input-user-email" placeholder="Email">
-    </div>
+           <div class="field">
+              <div class="ui left icon input">
+                <i class="lock icon"></i>
+                <input type="password" name="password" maxlength="255" id="input-user-password" placeholder="Senha">
+            </div>
+          </div>
 
-    <div class="required field">
-      <input type="password" name="password" maxlength="255" id="input-user-password" placeholder="Senha">
+          <button class="ui centered large pink right labeled icon submit button">
+            <i class="right arrow icon"></i>
+            Cadastrar
+          </button>
+        </div>
+        <div class="ui error message"></div>
+        <p id="result"></p>
+      </form>
     </div>
-
-    <input class="ui submit button" type="submit" value="Cadastrar">
-  </form>
-  <p id="result"></p>
+  </div>
 </body>
 
 <script>
@@ -54,5 +99,5 @@
   });
   
 </script>
-
 </html>
+

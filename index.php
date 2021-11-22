99be6456a7767a2b55f8a15b5dccc6371718cfdd
felipe <36082241+byuufx@users.noreply.github.com>
2021-11-22 00:26:57 -0300

    explanation
    update

--- index.php
+++ index.php
@@ -31,6 +31,10 @@ $products = mysqli_fetch_assoc($result);
          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
+
+      <!-- vai ter que ser feito algo em JS aqui, criar o HTML direto com PHP
+      não funciona. Infelizmente minhas capacidades 
+      em programação não chegam nesse nível ainda. Boa sorte. -->
       <div class="carousel-inner">
          <div class="carousel-item active">
             <img src="img/slides/0.jpg" class="d-block w-100" alt="...">
